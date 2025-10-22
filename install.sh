be2d64cf0233fcca42887a52f0d513a9375778ac
Mohammed Bilal N S <bilalnsmuhammed@gmail.com>
2025-10-22 23:35:54 +0530

    fix wrong config directory in script

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ PACKAGE_SRC="$(pwd)/battwatch"
 INSTALL_DIR="/opt/battwatch"
 BIN_LAUNCHER="/usr/bin/battwatch"
 USER_SERVICE_DIR="$HOME/.config/systemd/user"  # User service location
+CONFIG_DIR="/etc/battwatch"
+
 
 set -e
 
@@ -19,7 +21,7 @@ if [[ "$1" == "--uninstall" ]]; then
     systemctl --user disable battwatch.service || true
     sudo rm -f "$BIN_LAUNCHER"
     sudo rm -rf "$INSTALL_DIR"
-    rm -rf "$CONFIG_DIR"
+    sudo rm -rf "$CONFIG_DIR"
     rm -f "$USER_SERVICE_DIR/battwatch.service"
     systemctl --user daemon-reload
     echo "Uninstalled successfully."
@@ -34,9 +36,9 @@ sudo cp -r "$PACKAGE_SRC" "$INSTALL_DIR/"
 echo "Copied package to $INSTALL_DIR"
 
 # --- Ensure default config exists in package ---
-sudo mkdir -p "$INSTALL_DIR/battwatch/config"
-sudo cp configs/config.toml "$INSTALL_DIR/battwatch/config/default_config.toml"
-echo "Copied default config to package directory"
+sudo mkdir -p "$CONFIG_DIR"
+sudo cp configs/config.toml "$CONFIG_DIR/config.toml"
+echo "Copied default config to $CONFIG_DIR/config.toml"
 
 # --- Create launcher script ---
 echo "Creating launcher script at $BIN_LAUNCHER"
